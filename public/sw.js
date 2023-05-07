a902ff6d4d9c314877b3b6838eae2e271259604b
Link0w0 <66062268+LinkTheCoder@users.noreply.github.com>
2023-05-07 15:00:25 +0200

    update

--- public/sw.js
+++ public/sw.js
@@ -1,78 +1,66 @@
-var CACHE_STATIC_NAME = 'static-v1.0.1';
-var CACHE_DYNAMIC_NAME ='dynamic-v1.0.1';
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+// Customize this with a different URL if needed.
+const OFFLINE_URL = "offline.html";
 
-self.addEventListener('install', function(event){
-    console.log('[Service Worker] Install Service Worker', event);
-    event.waitUntil(
-        caches.open(CACHE_STATIC_NAME)
-        .then(function(cache){
-            console.log('[Service Worker] Precach AppShell');
-            cache.addAll([
-                '/',
-                'index.html',
-                'regsw.js',
-                '/App.jsx',
-                '/index.css',
-                './src/components/CardPWA.jsx',
-                './components/Dinner.jsx',
-                './components/FindUs.jsx',
-                './components/Footer.jsx',
-                './components/Hero.jsx',
-                './components/Lunch.jsx',
-                './components/Navbar.jsx',
-                './data/data.js',
-                'index.html',
-                'images/Azteka.jpg',
-                'images/BarnPizza.jpg',
-                'images/Bolognese.jpg',
-                'images/Calzone.jpg',
-                'images/Falafel.jpg',
-                'images/HeroImage.jpg',
-                'images/Husmakost.jpg',
-                'images/Italy.jpg',
-                'images/Kebab.jpg',
-                'images/KebabLunch.jpg',
-                'images/Margherita.jpg',
-                'images/NewYork.jpg',
-                'images/Romana.jpg',
-                'images/Sallad.jpg',
-                'images/SalladTallrik.jpg',
-                'images/Skrovmal.jpg',
-                'images/Svamp.jpg'
-            ]);
-        })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      // Setting {cache: 'reload'} in the new request ensures that the
+      // response isn't fulfilled from the HTTP cache; i.e., it will be
+      // from the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event){
-    console.log('[Service Worker] Activate Service Worker', event);
-    event.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
-                    console.log('[Service Worker] Delete old caches', key);
-                    caches.delete(key);
-                }
-            }));
-        })
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
-    //console.log('[Service Worker] Hämtar något med fetch', event);
-    event.respondWith(async function() {
-        const cachedResponse = await caches.match(event.request);
-        if(cachedResponse) return cachedResponse;
-        return fetch(event.request)
-        .then(function(res) {
-            return caches.open(CACHE_DYNAMIC_NAME)
-            .then(function(cache) {
-                cache.put(event.request.url, res.clone());
-                return res;
-            });
-        })
-        .catch(function(error){
+self.addEventListener("fetch", (event) => {
+  // Only call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's
+          // supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
 
-        });
-    }());
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is
+          // likely due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
 });
