2cc51af2c523f46b3adf3891692f6a87d3a99fae
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2023-05-28 10:32:26 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
 <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
 
 <div className="rounded overflow-hidden">
-  <div className="px-6 py-4 text-left">
-    <div className="font-normal text-6xl mb-2 sm: text-5xl">PIZZERIA & RESTAURANG</div>
-    <p className="font-black text-6xl sm: text-5xl">
+  <div className="md:px-8 sm: px-0 py-4 text-left">
+    <div className="font-normal md:text-6xl mb-2 sm: text-5xl">PIZZERIA & RESTAURANG</div>
+    <p className="font-black md:text-6xl sm: text-5xl">
 ÖST PÅ STAN
     </p>
   </div>
