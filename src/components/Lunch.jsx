56752c488b42be4dacd3afb24ee81c5c30fca0e6
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2023-08-11 12:23:43 +0200

    update

--- src/components/Lunch.jsx
+++ src/components/Lunch.jsx
@@ -9,9 +9,9 @@ const Lunch = () => {
   </h1>
   
     <div className='max-w-[1640px] mx-auto p-4 grid md:grid-cols-3 gap-6'>
-    <CardLunch title="KEBAB/PIZZATALLRIK" time="10/12 (10:30 - 15:00)" price="90" bg="/images/KebabLunch.jpg" alt="kebab" loading="lazy"/>  
-    <CardLunch title="SALLADSTALLRIK" time="10/14 (10:30 - 15:00)" price="70" bg="/images/SalladTallrik.jpg" alt="salladstallrik" loading="lazy"/>  
-    <CardLunch title="HUSMANSKOSTTALRK" time="10/16 (10:30 - 15:00)" price="70" bg="/images/Husmakost.jpg" alt="husmakost" loading="lazy"/>  
+    <CardLunch title="KEBAB/PIZZATALLRIK" time="10/12 (10:30 - 15:00)" price="90" bg="/images/KebabLunch.jpg" alt="kebab" loading="lazy" width="200" height="200"/>
+    <CardLunch title="SALLADSTALLRIK" time="10/14 (10:30 - 15:00)" price="70" bg="/images/SalladTallrik.jpg" alt="salladstallrik" loading="lazy" width="200" height="200"/>    
+    <CardLunch title="HUSMANSKOSTTALRK" time="10/16 (10:30 - 15:00)" price="70" bg="/images/Husmakost.jpg" alt="husmakost" loading="lazy" width="200" height="200"/>  
     </div>
     <p className='pb-8 text-center font-medium text-yellow-400'>* STUDENT & PENSIONERSRABATT 10%</p>
     </div>
